f8a4b3750a17dd10d4bb982031b5537fe23e4f24
shah israil <shahisrail134@gmail.com>
2023-08-18 17:32:07 +0600

    first commit

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -13,60 +13,109 @@ function calculateTriangaleArea() {
     const height = parseFloat(triangleHeightText)
     console.log(height);
 
+    if (isNaN(base) || isNaN(height)) {
+        alert('please insert a number')
+        return;
+    }
 
     const area = 0.5 * base * height
     // console.log(area);
-
+    addtoCalculatoinEntry('triangle', area)
     // show triangel area
 
     const triangleArea = document.getElementById('triangle-area')
     triangleArea.innerText = area
 }
 
+
+// function calculateRectangleArea() {
+//     // get reactangale vase value
+//     const rectangleWidth = document.getElementById('rectangle-width')
+//     const rectangleWidthText = rectangleWidth.value
+//     const width = parseFloat(rectangleWidthText)
+
+
+//     const rectangleLength = document.getElementById('rectangle-length')
+//     const rectangleLengthText = rectangleLength.value
+//     const length = parseFloat(rectangleLengthText)
+//     // validad input
+//     if (isNaN(width) || isNaN(length)) {
+//         alert('please insert a number')
+//         return;
+//     }
+
+//     const Area = width * length
+//     const rectangleArea = document.getElementById('rectangle-area')
+//     rectangleArea.innerText = Area;
+//     // addtoCalculatoinEntry('rectangle', area)`y
+
+
+
+// }
+// reuse functoin 
 function calculateRectangleArea() {
-    // get reactangale vase value
-    const rectangleWidth = document.getElementById('rectangle-width')
-    const rectangleWidthText = rectangleWidth.value
-    const width = parseFloat(rectangleWidthText)
-
-
-    const rectangleLength = document.getElementById('rectangle-length')
-    const rectangleLengthText = rectangleLength.value
-    const length = parseFloat(rectangleLengthText)
-
-    const Area = width * length
-
-    const rectangleArea = document.getElementById('rectangle-area')
-    rectangleArea.innerText = Area
-
+    const width = getInputValue('rectangle-width')
+    const length = getInputValue('rectangle-length')
+    if(isNaN(width)||isNaN(length)){
+        alert('please insert a number')
+        return;
+    }
+    const area = width * length
+    setELementText('rectangle-area', area)
+    addtoCalculatoinEntry('rectangle', area)
 }
 
-// reuse functoin 
+
 function calculateParallelogramArea() {
     const base = getInputValue('parallelogram-base')
     const height = getInputValue('parallelogram-height')
+    if (isNaN(base) || isNaN(height)) {
+        alert('please insert a number')
+        return;
+    }
     const area = base * height
     setELementText('parallelogram-area', area)
+
+    // add to calcuolatoin entry
+    addtoCalculatoinEntry('parallelogram', area)
 }
 
 function calculateEllipseArea() {
     const major = getInputValue('ellipse-major-radius')
     const minor = getInputValue('ellipse-minor-radius')
+
+    if (isNaN(major) || isNaN(minor)) {
+        alert('please insert a number')
+        return;
+    }
+
     const area = 3.14 * major * minor
+
     const areaTwoDecimal = area.toFixed(2)
-    setELementText('ellipse-area', area,areaTwoDecimal)
+    setELementText('ellipse-area', area, areaTwoDecimal)
+    addtoCalculatoinEntry('ellipse', area, areaTwoDecimal)
 }
 function calculatePentagonArea() {
-    const PentagonMajor =  getInputValue('Pentagon-major-radius')
+    const PentagonMajor = getInputValue('Pentagon-major-radius')
     const PentagonMinor = getInputValue('Pentagon-minor-radius')
-    const area = 0.5* PentagonMajor * PentagonMinor
-    setELementText('Pentagon-area',area)
+    if (isNaN(PentagonMajor) || isNaN(PentagonMinor)) {
+        alert('please insert a number')
+        return;
+    }
+    const area = 0.5 * PentagonMajor * PentagonMinor
+    addtoCalculatoinEntry('Pentagon', area)
+    setELementText('Pentagon-area', area)
 }
 function calculateRhombusArea() {
-    const RhombusMajor =  getInputValue('Rhombus-major-radius')
+    const RhombusMajor = getInputValue('Rhombus-major-radius')
     const RhombusMinor = getInputValue('Rhombus-minor-radius')
-    const area = 0.5* RhombusMajor * RhombusMinor
-    setELementText('Rhombus-area',area)
+    if (isNaN(RhombusMajor) || isNaN(RhombusMinor)) {
+        alert('please insert a number')
+        return;
+    }
+    const area = 0.5 * RhombusMajor * RhombusMinor
+    addtoCalculatoinEntry('Rhombus', area)
+    setELementText('Rhombus-area', area)
 }
 
 // reusable get input value feild number
@@ -81,4 +130,15 @@ function getInputValue(fieldId) {
 function setELementText(elemntId, area) {
     const element = document.getElementById(elemntId)
     element.innerText = area;
+}
+
+// add to calculate entry
+function addtoCalculatoinEntry(areaTYpe, area) {
+    console.log('adding soon')
+    const calculatoinEntry = document.getElementById('calculatoin-entry')
+    const count = calculatoinEntry.childElementCount
+    const p = document.createElement('p')
+    p.classList.add('my-4')
+    p.innerHTML = `${count + 1} ${areaTYpe} ${area} cm <Sup>2</Sup> <button class="btn btn-success">Covert to cm <sup>2</sup> </button>`
+    calculatoinEntry.appendChild(p)
 }
